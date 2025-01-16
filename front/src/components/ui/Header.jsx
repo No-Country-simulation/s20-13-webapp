35d412e9d1501ed4d2429222c59c9e969a5cb03a
fedecrer24 <fedeecrer@gmail.com>
2025-01-16 10:55:06 -0300

    navbar

--- front/src/components/ui/Header.jsx
+++ front/src/components/ui/Header.jsx
@@ -2,6 +2,22 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <header>
+    <div class="navbar">
+      
+      <div class="logo" onclick="logoClick()"><img src="Logo.png" alt=""/></div>
+  
+     
+      <div class="menu">
+        <a href="#">Soy dueño</a>
+        <a href="#">Soy cuidador</a>
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Services</a>
+        <a href="#">Contact</a>
+      </div>
+    </div>
+  </header>
+    
   )
 }

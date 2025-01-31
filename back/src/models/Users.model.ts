50b463964a0088af8add38c02fed3e1bfdb3e197
Tolomatico <ballesty.t@gmail.com>
2025-01-31 18:27:58 -0300

    Add Model

--- back/src/models/Users.model.ts
+++ back/src/models/Users.model.ts
@@ -15,20 +15,9 @@ interface Cost {
   day: number
 }
 
-<<<<<<< HEAD
 export enum PetType{
   DOG="dog",
   CAT="cat"
-=======
-export enum AvailabilityDay {
-  MONDAY = "monday",
-  TUESDAY = "tuesday",
-  WEDNESDAY = "wednesday",
-  THURSDAY = "thursday",
-  FRIDAY = "friday",
-  SATURDAY = "saturday",
-  SUNDAY = "sunday",
->>>>>>> 050f223ebe8ba1a48774885bca70113f8f9811d5
 }
 
 
@@ -129,7 +118,6 @@ const userSchema: Schema = new Schema<IUser>(
         type: Number,
         default: null
       }
-
     },
     isActive: {
       type: Boolean,

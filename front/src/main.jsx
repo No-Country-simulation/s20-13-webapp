66bfbc12e9f4e1799440d7fe291ea6cff458c179
Ailen Franco <ailenfranco43@gmail.com>
2025-02-03 10:46:25 -0300

    refactor: change endpoint from /users to /caretaker/filter in getCaretakers

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import Router from './Router.jsx'
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { UserProvider } from './context/userContext.jsx';

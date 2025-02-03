31b44d97cccdbf3a44d9d5f775a3e333e253df61
Ailen Franco <ailenfranco43@gmail.com>
2025-02-03 10:43:36 -0300

    fix: searchResults

--- front/src/views/SearchResults.jsx
+++ front/src/views/SearchResults.jsx
@@ -1,40 +1,50 @@
 import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import Caretakers from "../components/ui/Caretakers"; 
+import Caretakers from "../components/ui/Caretakers";
 import SearchBar from "../components/ui/SearchBar";
 import SearchFilter from "../components/ui/SearchFilter";
 
 export default function SearchResults() {
   const location = useLocation();
-  const queryParams = new URLSearchParams(location.search);
-
-  const service = queryParams.get("service");
-  const zone = queryParams.get("zone");
-
-  const neighborhood = {
-    neighborhood: zone || "",
-  };
 
   const [caretakersCount, setCaretakersCount] = useState(0);
+  const [filters, setFilters] = useState({
+    service: "",
+    zone: "",
+    pets: "",
+    neighborhood: "",
+    reviews: "",
+    maxPrice: "",
+    order: "",
+  });
 
-  
+  useEffect(() => {
+    const queryParams = new URLSearchParams(location.search);
+    const service = queryParams.get("service");
+    const zone = queryParams.get("zone");
+
+    setFilters((prevFilters) => ({
+      ...prevFilters,
+      service: service || "",
+      zone: zone || "",
+    }));
+  }, [location.search]);
+
   const updateCaretakersCount = (count) => {
     setCaretakersCount(count);
   };
 
-  // Definir la función updateFilters
-  const updateFilters = (filters) => {
-    console.log("Updated filters:", filters);
-    // lógica adicional para manejar los filtros actualizados
+  const updateFilters = (newFilters) => {
+    setFilters((prevFilters) => ({ ...prevFilters, ...newFilters }));
   };
 
   return (
-    <div>
-      <SearchBar className="search-results-container"/> 
-      <h1>Resultados de la búsqueda</h1>
-      <p>Cantidad de resultados: {caretakersCount}</p>
-      <SearchFilter updateFilters={updateFilters} /> {/* Pasar la función updateFilters como prop */}
-      <Caretakers neighborhood={neighborhood} updateCaretakersCount={updateCaretakersCount} />
+    <div className="search-results-container">
+      <SearchBar updateFilters={updateFilters} />
+      <h2>Resultados:</h2>
+      <p>{caretakersCount} personas</p>
+      <SearchFilter updateFilters={updateFilters} />
+      <Caretakers filters={filters} updateCaretakersCount={updateCaretakersCount} cardClassName="caretaker-card-search" />
     </div>
   );
 }
